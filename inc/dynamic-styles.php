069c01c431bd0c4d244f6a69fd1d59be377feacf
Jayson Brown <jayb3252@gmail.com>
2023-11-08 15:56:24 -0500

    Building theme customizations

--- inc/dynamic-styles.php
+++ inc/dynamic-styles.php
@@ -3,10 +3,25 @@ function themetest_customize_css()
 {
     ?>
          <style type="text/css">
+               #masthead {
+                background: linear-gradient(<?php echo get_theme_mod('header_backgroundcolorDirection', '45'); ?>deg,
+                <?php echo get_theme_mod('header_backgroundcolor'); ?>, 
+                <?php echo get_theme_mod('header_backgroundcolor2'); ?>);  
+                border-bottom: <?php echo get_theme_mod('header_borderSize'); ?>px solid
+                <?php echo get_theme_mod('header_borderColor'); ?>;
+                font-size: <?php echo get_theme_mod('header_fontSize', 'Large'); ?>px;
+                 
+               }
                #masthead div ul li a {
-                color: <?php echo get_theme_mod('header_textcolor', '#000000'); ?>;
+                color: <?php echo get_theme_mod('header_textcolor');?>;
+
+            }
+               footer {
+                color: <?php echo get_theme_mod('footer_textcolor'); ?>;
+                background-color: <?php echo get_theme_mod('footer_backgroundcolor'); ?>;
             }
          </style>
     <?php
 }
 add_action( 'wp_head', 'themetest_customize_css');
+add_action( 'wp_footer', 'themetest_customize_css');
--- inc/themetest-customizer.php
+++ inc/themetest-customizer.php
@@ -7,21 +7,168 @@
 
 function themetest_customize_register( $wp_customize ) {
     $wp_customize->add_setting( 'header_textcolor' , array(
-        'default'   => '#000000',
+        'default'   => '#FFFFFF',
         'transport' => 'refresh',
         'sanitize_callback' => 'sanitize_hex',
      ));
     
-    $wp_customize->add_section('themetest_new_section_name', array(
-        'title'      => __( 'Header Text Color', 'themetest' ),
+     $wp_customize->add_setting( 'header_backgroundcolor' , array(
+        'default'   => '#FF0000',
+        'transport' => 'refresh',
+        'sanitize_callback' => 'sanitize_hex',
+     ));
+
+     $wp_customize->add_setting( 'header_backgroundcolor2' , array(
+        'default'   => '#000000',
+        'transport' => 'refresh',
+        'sanitize_callback' => 'sanitize_hex',
+     ));
+
+     $wp_customize->add_setting( 'header_backgroundcolorDirection' , array(
+        'default'   => '45',
+        'transport' => 'refresh',
+        'sanitize_callback' => 'sanitize_key'
+        
+     ));
+
+     $wp_customize->add_setting( 'header_borderSize' , array(
+        'default'   => 'small',
+        'transport' => 'refresh',
+        'sanitize_callback' => 'sanitize_key',
+        
+     ));
+
+     $wp_customize->add_setting( 'header_borderColor' , array(
+        'default'   => '#0000FF',
+        'transport' => 'refresh',
+        'sanitize_callback' => 'sanitize_hex',
+     ));
+
+     $wp_customize->add_setting( 'header_fontSize' , array(
+        'default'   => '30',
+        'transport' => 'refresh',
+        'sanitize_callback' => 'sanitize_key'
+     ));
+
+    $wp_customize->add_section('themetest_header_settings', array(
+        'title'      => __( 'Header Settings', 'themetest' ),
         'priority'   => 1,
     ));
     
+    
+
+   
     $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_textcolor', array(
         'label'      => __( 'Header Text Color', 'themetest' ),
-        'section'    => 'themetest_new_section_name',
+        'section'    => 'themetest_header_settings',
         'settings'   => 'header_textcolor',
+      
+    )));
+
+    
+
+    
+
+
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_backgroundcolor', array(
+        'label'      => __( 'Header Background Color', 'themetest' ),
+        'section'    => 'themetest_header_settings',
+        'settings'   => 'header_backgroundcolor',
+       
+    )));
+
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_backgroundcolor2', array(
+        'label'      => __( 'Header Background Color 2', 'themetest' ),
+        'section'    => 'themetest_header_settings',
+        'settings'   => 'header_backgroundcolor2',
+     
+    )));
+
+    $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'header_backgroundcolorDirection', array(
+        'label'    => __( 'Gradient Direction', 'themetest' ),
+        'section'  => 'themetest_header_settings',
+        'settings' => 'header_backgroundcolorDirection',
+        'type'     => 'number',
+        'input_attrs' => array(
+            'min'  => 1,
+            'max'  => 360,
+            'step' => 1,
+        
+        ),
+        'description' => __( 'Enter a number between 1 and 360 for gradient direction. Default is 45 degrees.', 'themetest' ),
     ) ) );
+
+    $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'header_borderSize', array(
+        'label'      => __( 'Header Border Size', 'themetest' ),
+        'section'    => 'themetest_header_settings',
+        'settings'   => 'header_borderSize',
+        'type' => 'select',
+        'choices' => array(
+            0 => __( 'None', 'themetest' ),
+            5 => __( 'Very small', 'themetest' ),
+            10 => __( 'Small', 'themetest' ),
+            20 => __( 'Medium', 'themetest' ),
+            30 => __( 'Large', 'themetest' ),
+        ),
+
+     
+    )));
+
+       
+
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_borderColor', array(
+        'label'      => __( 'Header Border Color', 'themetest' ),
+        'section'    => 'themetest_header_settings',
+        'settings'   => 'header_borderColor',
+       
+    )));
+   
+    $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'header_fontSize', array(
+        'label'      => __( 'Header Font Size', 'themetest' ),
+        'section'    => 'themetest_header_settings',
+        'settings'   => 'header_fontSize',
+        'type' => 'select',
+        'choices' => array(
+            10 => __( 'Very small', 'themetest' ),
+            15 => __( 'Small', 'themetest' ),
+            20 => __( 'Medium', 'themetest' ),
+            30 => __( 'Large', 'themetest' ),
+            40 => __( 'Very large', 'themetest' ),
+        ),
+
+       
+    )));
+
+    $wp_customize->add_setting( 'footer_textcolor' , array(
+        'default'   => '#FFFFFF',
+        'transport' => 'refresh',
+        'sanitize_callback' => 'sanitize_hex',
+     ));
+
+     $wp_customize->add_setting( 'footer_backgroundcolor' , array(
+        'default'   => '#2d2d2d',
+        'transport' => 'refresh',
+        'sanitize_callback' => 'sanitize_hex',
+     ));
+
+    $wp_customize->add_section('themetest_footer_settings', array(
+        'title'      => __( 'Footer Settings', 'themetest' ),
+        'priority'   => 2,
+    ));
+
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_textcolor', array(
+        'label'      => __( 'Footer Text Color', 'themetest' ),
+        'section'    => 'themetest_footer_settings',
+        'settings'   => 'footer_textcolor',
+        'priority'   => 1,
+    )));
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_backgroundcolor', array(
+        'label'      => __( 'Footer Background Color', 'themetest' ),
+        'section'    => 'themetest_footer_settings',
+        'settings'   => 'footer_backgroundcolor',
+        'priority'   => 2,
+    )));
+
  }
 
 
